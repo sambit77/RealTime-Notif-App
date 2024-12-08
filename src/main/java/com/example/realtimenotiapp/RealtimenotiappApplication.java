92acf4bc0767c74d4fadb8084872425a92aa6b13
Sambit Pradhan <sambitgulu@gmail.com>
2024-12-09 00:30:24 +0530

    base commit

---
+++ src/main/java/com/example/realtimenotiapp/RealtimenotiappApplication.java
@@ -0,0 +1,13 @@
+package com.example.realtimenotiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimenotiappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimenotiappApplication.class, args);
+	}
+
+}
